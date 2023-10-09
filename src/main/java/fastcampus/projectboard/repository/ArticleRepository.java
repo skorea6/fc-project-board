45e47b30bc5eb7811141ff2befac0c235c23f548
skorea6 <skorea6@naver.com>
2023-10-09 22:04:31 +0900

    #21 - 비지니스 로직 구현
    테스트를 토대로 게시판 서비스에 필요한 비지니스 로직 구현 완료

--- src/main/java/fastcampus/projectboard/repository/ArticleRepository.java
+++ src/main/java/fastcampus/projectboard/repository/ArticleRepository.java
@@ -18,8 +18,13 @@ public interface ArticleRepository extends
         QuerydslPredicateExecutor<Article>,
         QuerydslBinderCustomizer<QArticle> {
 
-    Page<Article> findByTitle(String title, Pageable pageable);
-    
+    Page<Article> findByTitleContaining(String title, Pageable pageable);
+    Page<Article> findByContentContaining(String content, Pageable pageable);
+    Page<Article> findByUserAccount_IdContaining(String userId, Pageable pageable);
+
+    Page<Article> findByUserAccount_NicknameContaining(String nickname, Pageable pageable);
+    Page<Article> findByHashtag(String hashtag, Pageable pageable);
+
     @Override
     default void customize(QuerydslBindings bindings, QArticle root){
         bindings.excludeUnlistedProperties(true);
