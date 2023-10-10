d906a507debefdaf691689020345e33ccda69817
skorea6 <skorea6@naver.com>
2023-10-10 23:32:07 +0900

    #28 - 도메인 업데이트: UserAccount 회원 ID에 유니크 키 추가
    회원 id로 로그인하고 유저 식별하므로, 당연히 uk 여야 한다.
    이 부분이 설계에서 반영되지 않았던 것을 발견
    테스트는 uk 적용으로 기존 `data.sql`의 테스트 데이터와
    중복이 발생하므로 `userId` 이름을 수정

--- src/test/java/fastcampus/projectboard/repository/JpaRepositoryTest.java
+++ src/test/java/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -47,7 +47,7 @@ class JpaRepositoryTest {
         //given
         long previousCount = articleRepository.count();
 
-        UserAccount userAccount = userAccountRepository.save(UserAccount.of("uno", "pw", null, null, null));
+        UserAccount userAccount = userAccountRepository.save(UserAccount.of("newUno", "pw", null, null, null));
         Article article = Article.of(userAccount, "new article", "new content", "#spring");
 
         //when
