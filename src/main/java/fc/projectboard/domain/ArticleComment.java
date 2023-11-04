e034fc7e2119a6edc64ba358e53519686bd9f22b
skorea6 <skorea6@naver.com>
2023-11-04 17:25:22 +0900

    #70 - 대댓글 도메인 설계
    대댓글 도메인 안에서 부모, 자식 관계를 설정하는 코드를 추가
    
    자식 댓글의 컬렉션 변화가 쿼리에 반영되게끔 cascading 규칙을 모두 적용
    
    단방향 연관관계 설정을 사용해보기로 함
    따라서 부모 댓글은 엔티티가 아닌 `Long` id를 직접 표현
    
    또한 자식 댓글을 추가할 수 있는 메소드 추가 제공

--- src/main/java/fc/projectboard/domain/ArticleComment.java
+++ src/main/java/fc/projectboard/domain/ArticleComment.java
@@ -4,7 +4,10 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @ToString
@@ -29,6 +32,15 @@ public class ArticleComment extends AuditingFields{
     private UserAccount userAccount;
 
     @Setter
+    @Column(updatable = false)
+    private Long parentCommentId; // 부모 댓글 ID
+
+    @ToString.Exclude
+    @OrderBy("createdAt ASC")
+    @OneToMany(mappedBy = "parentCommentId", cascade = CascadeType.ALL)
+    private Set<ArticleComment> childComments = new LinkedHashSet<>();
+
+    @Setter
     @Column(nullable = false, length = 500)
     private String content; // 본문
 
@@ -37,14 +49,20 @@ public class ArticleComment extends AuditingFields{
 
     }
 
-    private ArticleComment(Article article, UserAccount userAccount, String content) {
+    private ArticleComment(Article article, UserAccount userAccount, Long parentCommentId, String content) {
         this.article = article;
         this.userAccount = userAccount;
+        this.parentCommentId = parentCommentId;
         this.content = content;
     }
 
     public static ArticleComment of(Article article, UserAccount userAccount, String content) {
-        return new ArticleComment(article, userAccount, content);
+        return new ArticleComment(article, userAccount, null, content);
+    }
+
+    public void addChildComment(ArticleComment child){
+        child.setParentCommentId(this.getId());
+        this.getChildComments().add(child);
     }
 
     @Override
