2baa422477f1d9f75db0ebf1ae5af8832f115417
skorea6 <skorea6@naver.com>
2023-11-05 14:32:26 +0900

    #72 - 대댓글 비즈니스 로직 구현
    - 부모/자식 관계를 만들면서 entity <-> dto 간 변환 로직 댓글 구현
    - 서비스에서 댓글과 대댓글을 저장하고 삭제하는 방법 구현
    - 댓글과 대댓글을 db 데이터로부터 변환할 때 서로 계층 구조를 만들고 각각 정렬하는 과정 구현

--- src/main/java/fc/projectboard/dto/request/ArticleCommentRequest.java
+++ src/main/java/fc/projectboard/dto/request/ArticleCommentRequest.java
@@ -6,15 +6,20 @@ import fc.projectboard.dto.UserAccountDto;
 /**
  * DTO for {@link fc.projectboard.domain.ArticleComment}
  */
-public record ArticleCommentRequest(Long articleId, String content) {
+public record ArticleCommentRequest(Long articleId, Long parentCommentId, String content) {
     public static ArticleCommentRequest of(Long articleId, String content) {
-        return new ArticleCommentRequest(articleId, content);
+        return ArticleCommentRequest.of(articleId, null, content);
+    }
+
+    public static ArticleCommentRequest of(Long articleId, Long parentCommentId, String content) {
+        return new ArticleCommentRequest(articleId, parentCommentId, content);
     }
 
     public ArticleCommentDto toDto(UserAccountDto userAccountDto) {
         return ArticleCommentDto.of(
                 articleId,
                 userAccountDto,
+                parentCommentId,
                 content
         );
     }
--- src/main/java/fc/projectboard/dto/response/ArticleWithCommentsResponse.java
+++ src/main/java/fc/projectboard/dto/response/ArticleWithCommentsResponse.java
@@ -1,11 +1,12 @@
 package fc.projectboard.dto.response;
 
+import fc.projectboard.dto.ArticleCommentDto;
 import fc.projectboard.dto.ArticleWithCommentsDto;
 import fc.projectboard.dto.HashtagDto;
 
 import java.time.LocalDateTime;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public record ArticleWithCommentsResponse(
@@ -40,10 +41,31 @@ public record ArticleWithCommentsResponse(
                 dto.userAccountDto().email(),
                 nickname,
                 dto.userAccountDto().userId(),
-                dto.articleCommentDtos().stream()
-                        .map(ArticleCommentResponse::from)
-                        .collect(Collectors.toCollection(LinkedHashSet::new))
+                organizeChildComments(dto.articleCommentDtos())
         );
+    }
+
+    private static Set<ArticleCommentResponse> organizeChildComments(Set<ArticleCommentDto> dtos) {
+        Map<Long, ArticleCommentResponse> map = dtos.stream()
+                .map(ArticleCommentResponse::from) // ArticleCommentResponse 로 mapping (이 과정에서 자식 댓글들이 시간 ASC로 정렬)
+                .collect(Collectors.toMap(ArticleCommentResponse::id, Function.identity())); // id, ArticleCommentResponse(자신)으로 Map
+
+        map.values().stream()
+                .filter(ArticleCommentResponse::hasParentComment) // 자식 댓글들만 filter
+                .forEach(comment -> {
+                    ArticleCommentResponse parentComment = map.get(comment.parentCommentId()); // 부모 댓글 찾기
+                    parentComment.childComments().add(comment); // 부모 댓글의 자식 댓글 리스트에 추가
+                });
+
+        return map.values().stream()
+                .filter(comment -> !comment.hasParentComment()) // 부모 댓글들만 filter
+                .collect(Collectors.toCollection(() ->
+                        new TreeSet<>(Comparator
+                                .comparing(ArticleCommentResponse::createdAt)
+                                .reversed() // [1] 최신순으로 정렬. DESC
+                                .thenComparingLong(ArticleCommentResponse::id) // [2] id로 정렬. ASC
+                        )
+                ));
     }
 
 }
